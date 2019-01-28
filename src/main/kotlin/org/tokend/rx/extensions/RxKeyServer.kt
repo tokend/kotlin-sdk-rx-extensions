517e13de917533c81d1f8b3f1b6053a01d01f7eb
Aleksandr <al.ivanchukov@gmail.com>
2019-01-28 12:59:25 +0200

    Remove useless methods according to KeyServer updates

--- src/main/kotlin/org/tokend/rx/extensions/RxKeyServer.kt
+++ src/main/kotlin/org/tokend/rx/extensions/RxKeyServer.kt
@@ -2,7 +2,6 @@
 
 package org.tokend.rx.extensions
 
-import io.reactivex.Completable
 import io.reactivex.Single
 import org.tokend.sdk.keyserver.KeyServer
 import org.tokend.sdk.keyserver.models.*
@@ -21,56 +20,6 @@ fun KeyServer.getWalletInfoSingle(login: String,
                                   isRecovery: Boolean = false): Single<WalletInfo> {
     return Single.defer {
         Single.just(getWalletInfo(login, password, isRecovery))
-    }
-}
-
-/**
- * @return lazy [Single] with login params request result
- *
- * @see KeyServer.getLoginParams
- */
-@JvmOverloads
-fun KeyServer.getLoginParamsSingle(login: String? = null,
-                                   isRecovery: Boolean = false): Single<LoginParams> {
-    return Single.defer {
-        Single.just(getLoginParams(login, isRecovery))
-    }
-}
-
-/**
- * @return lazy [Single] with wallet data request result
- *
- * @see KeyServer.getWalletData
- */
-fun KeyServer.getWalletDataSingle(walletId: String): Single<WalletData> {
-    return Single.defer {
-        Single.just(getWalletData(walletId))
-    }
-}
-
-
-/**
- * @return lazy [Completable] for wallet save request
- *
- * @see KeyServer.saveWallet
- */
-fun KeyServer.saveWalletCompletable(walletData: WalletData): Completable {
-    return Completable.defer {
-        saveWallet(walletData)
-        Completable.complete()
-    }
-}
-
-
-/**
- * @return lazy [Completable] for wallet update request
- *
- * @see KeyServer.updateWallet
- */
-fun KeyServer.updateWalletCompletable(walletId: String, walletData: WalletData): Completable {
-    return Completable.defer {
-        updateWallet(walletId, walletData)
-        Completable.complete()
     }
 }
 
